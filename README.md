50a005646a0e48e9032ae3819acec711d36a39c0
Orach_Dmitriy <orachdv@gmail.com>
2019-06-10 20:56:08 +0300

    Photo Gallery

---
+++ README.md
@@ -0,0 +1,34 @@
+# JavaScript-praktika
+
+Here are my works
+
+Here you can see:
+
+
+1)Writing an interface for a news portal
+
+2)Exchange Rate Request
+
+3)Weather Requests
+
+4)Modal windows
+
+5)Authorization and user registration
+
+6)Validation of forms
+
+7)Downloading news
+
+8)Accordions
+
+9)Sliders
+
+10)Cookie timer for displaying banners
+
+11)Search organization
+
+12)Mobile adaptation
+
+13)Sandwich loaf menu
+
+14)Refactoring and code optimization
--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
-alert("Hello!");
+
+i = 0;
+let images = ["img/52553778_2239802329411753_1615837192981541835_n.jpg", "img/53086310_309860299674285_6230656625640732532_n.jpg", "img/53238347_378266366340523_1975914855758364120_n.jpg", "img/photo_2019-02-04_22-28-37.jpg", "img/photo_2019-02-12_19-13-15.jpg"];
+
+let left = document.getElementById('left');
+let right = document.getElementById('right');
+
+right.addEventListener("click", function zamenaPhoto(){
+
+let photo = document.getElementById('photo');
+
+photo.src = images[i+1];
+i++;
+if (i>=4){
+	i = 0;
+}
+});
+
+left.addEventListener("click", function zamenaPhoto(){
+
+let photo = document.getElementById('photo');
+
+photo.src = images[i+1];
+i++;
+if (i>=4){
+	i = 0;
+}
+});
